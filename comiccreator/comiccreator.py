8dd1a21003e92659950d5107cc424f11aaf8aa58
Nitesh Bawane <Nitesh.Bawane@wdc.com>
2018-02-04 16:53:40 +0530

    Comic creator
    example to use layouts

--- comiccreator/comiccreator.py
+++ comiccreator/comiccreator.py
@@ -1,5 +1,12 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.lang.builder import Builder
+
+
+Builder.load_file('tools.kv')
+Builder.load_file('drawingspace.kv')
+Builder.load_file('controls.kv')
+
 
 class ComicCreator(AnchorLayout):
 	pass
